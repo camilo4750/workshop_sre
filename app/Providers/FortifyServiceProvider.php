0f9f747f98825e9b7499193d155dbb7932d349b2
camilo Alarcon <camilo.alarcon@savne.net>
2024-01-25 13:13:52 -0500

    Fix version adminlte3

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,7 +28,7 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::loginView('auth.login');
+        Fortify::loginView('vendor.adminlte.auth.login');
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
