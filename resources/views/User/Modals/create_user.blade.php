057d77f928edf1e972deff55b1a4de54789029a9
camilo Alarcon <camilo.alarcon@savne.net>
2024-02-19 14:56:56 -0500

    Advance create user

--- resources/views/User/Modals/create_user.blade.php
+++ resources/views/User/Modals/create_user.blade.php
@@ -1,18 +1,70 @@
-<div class="modal fade" id="createUser" data-bs-backdrop="static" data-bs-keyboard="false"
+<div class="modal fade bd-example-modal-lg" id="createUser" data-bs-backdrop="static" data-bs-keyboard="false"
      aria-labelledby="createUserLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="createUserLabel">Crear Usuario</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createUserLabel">Crear Usuario</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form @submit.prevent="storeUser">
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Primer Nombre"
+                                       v-model="createUser.firstName"  aria-label="firstName">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Segundo Nombre"
+                                       v-model="createUser.secondName" aria-label="secondName">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Primer Apellido"
+                                       v-model="createUser.firstSurname" aria-label="firstSurname">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Segundo apellido"
+                                       v-model="createUser.secondSurname" aria-label="secondSurname">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Telefono"
+                                       v-model="createUser.telephone" aria-label="Telephone">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <select name="typeUser" id="typeUser"  class="form-control"
+                                        v-model="createUser.typeUser" aria-label="typeUser">
+                                    <option value="" selected disabled>- Seleccionar Rol -</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Correo"
+                                       v-model="createUser.email" aria-label="email">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Contraseña"
+                                       v-model="createUser.password" aria-label="password">
+                            </div>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <label for="isActive" class="mb-0">Usuario activo?</label>
+                            <input type="checkbox" class="ml-2" name="isActive" id="isActive" v-model="createUser.isActive">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Crear Usuario</button>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
 </div>
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -8,22 +8,86 @@
     <div id="app">
         <div class="card border-color-1 p-2">
             <div class="d-flex justify-content-end">
-                <button type="button" class="btn btn-outline-secondary font-weight-bolder"
-                        data-bs-toggle="modal" data-bs-target="#createUser">
+                <button type="button" class="btn btn-outline-secondary font-weight-bolder" @click="openModalCreateUser">
                     Crear Usuario
                 </button>
             </div>
             @{{ message }}
+            <div class="">
+                <table id="tableUsers" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                    <tr class="text-center">
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Telefono</th>
+                        <th>Rol</th>
+                        <th>Correo</th>
+                        <th>Estado</th>
+                        <th>Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
         @include('User.Modals.create_user')
     </div>
 
     <script>
-
         const app = Vue.createApp({
             data() {
                 return {
+                    createUser: {
+                        firstName: '',
+                        secondName: '',
+                        firstSurname: '',
+                        secondSurname: '',
+                        telephone: '',
+                        typeUser: '',
+                        email: '',
+                        password: '',
+                        isActive: true
+                    },
                     message: 'Hello Vue!'
+                }
+            },
+            mounted(){
+                let tableUser = $('#tableUsers').DataTable({
+                    language: es_datatables,
+                });
+            },
+            methods: {
+                openModalCreateUser() {
+                    $('#createUser').modal('show');
+                },
+
+                storeUser() {
+                  fetch('{{route('register')}}', {
+                      method: 'POST',
+                      headers: {
+                          'Content-Type': 'application/json',
+                          'accept': 'application/json',
+                          'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                      },
+                      body: JSON.stringify({
+                        'data': this.createUser
+                      })
+                  }).then((res) => res.json())
+                      .then(response => {
+
+                      }).catch(
+                          err => alert('Error creating user: ' + err)
+                  );
                 }
             }
         });
