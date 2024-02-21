0414a9ca8b37304059724b2f817092ee935e7ea9
Miranda_Yuber <yuber.miranda@savne.net>
2024-02-21 10:57:19 -0500

    Add configuration permissions

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticate
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
     protected $table = 'users';
 
 
