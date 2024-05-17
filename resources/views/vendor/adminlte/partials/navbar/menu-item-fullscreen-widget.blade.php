45a9a3c1be23a1f92ed52967b279d262588d7744
camilo Alarcon <camilo.alarcon@savne.net>
2024-05-17 13:35:36 -0500

    Add section logout

--- resources/views/vendor/adminlte/partials/navbar/menu-item-fullscreen-widget.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-fullscreen-widget.blade.php
@@ -1,5 +1,19 @@
 <li class="nav-item">
-    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+    <a class="nav-link d-flex align-items-center" data-widget="fullscreen" href="#" role="button">
         <i class="fas fa-expand-arrows-alt"></i>
     </a>
+</li>
+
+<li class="nav-item">
+    <div class="dropdown">
+        <button class="btn text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle"></i>
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button class="dropdown-item d-flex align-items-center" type="submit">Cerrar session <i class="fas fa-power-off ml-2 text-red"></i></button>
+            </form>
+        </div>
+    </div>
 </li>
