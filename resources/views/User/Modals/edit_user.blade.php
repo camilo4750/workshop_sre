103286b36b096021dc590332fea2df864c482647
Camilo Alarcon <camilo4750@gmail.com>
2024-11-27 16:44:58 -0500

    Apply the optimizations made in the user module and make the relevant corrections

--- resources/views/User/Modals/edit_user.blade.php
+++ resources/views/User/Modals/edit_user.blade.php
@@ -1,10 +1,11 @@
 <div class="modal fade bd-example-modal-lg" id="editUserModal" data-bs-backdrop="static" data-bs-keyboard="false"
-     aria-labelledby="editUserLabel" aria-hidden="true">
+    aria-labelledby="editUserLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editUserLabel">Editar Usuario: @{{ editUser?.firstName + ' ' +
-                    editUser?.secondName }}</h5>
+                <h5 class="modal-title" id="editUserLabel">
+                    Editar Usuario: @{{ editUser?.fullName }}
+                </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -13,54 +14,26 @@
                 <div class="modal-body">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="form-group col-md-6">
-                                <input type="text" name="editFirstName" id="editFirstName"
-                                       class="form-control" placeholder="Primer Nombre"
-                                       v-model="editUser.firstName" aria-label="editFirstName">
+                            <div class="form-group col-md-12">
+                                <input type="text" name="editFirstName" id="editFirstName" class="form-control"
+                                    placeholder="Primer Nombre" v-model="editUser.fullName" aria-label="editFirstName">
                                 <span class="text-danger text-sm" v-if="fieldsStatusEdit.firstName">
-                                    @{{ fetchErrors?.firstName }}
+                                    @{{ fetchErrors?.fullName }}
                                 </span>
                             </div>
-                            <div class="form-group col-md-6">
-                                <input type="text" name="editSecondName" id="editSecondName"
-                                       class="form-control" placeholder="Segundo Nombre"
-                                       v-model="editUser.secondName" aria-label="editSecondName">
-                                <span class="text-danger text-sm" v-if="fieldsStatusEdit.secondName">
-                                    @{{ fetchErrors?.secondName }}
-                                </span>
-                            </div>
+                        
                         </div>
                         <div class="row">
                             <div class="form-group col-md-6">
-                                <input type="text" name="editFirstSurname" id="editFirstSurname"
-                                       class="form-control" placeholder="Primer Apellido"
-                                       v-model="editUser.firstSurname" aria-label="editFirstSurname">
-                                <span class="text-danger text-sm" v-if="fieldsStatusEdit.firstSurname">
-                                    @{{ fetchErrors?.firstSurname }}
-                                </span>
-                            </div>
-                            <div class="form-group col-md-6">
-                                <input type="text" name="editSecondSurname" id="editSecondSurname"
-                                       class="form-control" placeholder="Segundo apellido"
-                                       v-model="editUser.secondSurname" aria-label="editSecondSurname">
-                                <span class="text-danger text-sm" v-if="fieldsStatusEdit.secondSurname">
-                                    @{{ fetchErrors?.secondSurname }}
-                                </span>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-md-6">
-                                <input type="text" name="editTelephone" id="editTelephone"
-                                       class="form-control" placeholder="Telefono"
-                                       v-model="editUser.telephone" aria-label="editTelephone">
+                                <input type="text" name="editTelephone" id="editTelephone" class="form-control"
+                                    placeholder="Telefono" v-model="editUser.telephone" aria-label="editTelephone">
                                 <span class="text-danger text-sm" v-if="fieldsStatusEdit.telephone">
                                     @{{ fetchErrors?.telephone }}
                                 </span>
                             </div>
                             <div class="form-group col-md-6">
-                                <input type="text" name="editEmail" id="editEmail"
-                                       class="form-control" placeholder="Correo"
-                                       v-model="editUser.email" aria-label="editEmail">
+                                <input type="text" name="editEmail" id="editEmail" class="form-control"
+                                    placeholder="Correo" v-model="editUser.email" aria-label="editEmail">
                                 <span class="text-danger text-sm" v-if="fieldsStatusEdit.email">
                                     @{{ fetchErrors?.email }}
                                 </span>
@@ -68,8 +41,8 @@
                         </div>
                         <div class="d-flex align-items-center">
                             <label for="editIsActive" class="mb-0">Usuario activo?</label>
-                            <input type="checkbox" class="ml-2" name="editIsActive"
-                                   id="editIsActive" v-model="editUser.isActive">
+                            <input type="checkbox" class="ml-2" name="editIsActive" id="editIsActive"
+                                v-model="editUser.isActive">
                         </div>
                     </div>
                 </div>
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -4,81 +4,83 @@
     #tableUsers colgroup {
         display: none;
     }
+
+    .mx-3px {
+        padding-inline: 3px;
+    }
 </style>
 @section('content_header')
-    <div class="d-flex align-items-center">
-        <h1 class="font-weight-bold ml-3">Usuarios</h1>
-    </div>
+<div class="d-flex align-items-center">
+    <h1 class="font-weight-bold ml-3">Usuarios</h1>
+</div>
 @stop
 
 @section('content')
-    <div id="app">
-        <div class="card border-color-1 p-2">
-            <div class="d-flex justify-content-end mb-2     ">
-                <button type="button" class="btn btn-secondary font-weight-bolder" @click="openModalCreateUser">
-                    Crear Usuario
-                </button>
-            </div>
-            <div class="my-3" v-if="!isLoading">
-                <table id="tableUsers" class="table table-striped table-bordered table-hover dataTable"
-                       style="width: 100% !important;">
-                    <thead>
+<div id="app">
+    <div class="card border-color-1 p-2">
+        <div class="d-flex justify-content-end mb-2     ">
+            <button type="button" class="btn btn-color-dark font-weight-bolder" @click="openModalCreateUser">
+                Crear Usuario
+            </button>
+        </div>
+        <div v-if="!isLoading" class="my-3">
+            <table id="tableUsers" class="table table-striped table-bordered table-hover dataTable"
+                style="width: 100% !important;">
+                <thead>
                     <tr class="text-center">
-                        <th>Nombre</th>
-                        <th>Apellidos</th>
+                        <th>Nombre completo</th>
                         <th>Telefono</th>
                         <th>Correo</th>
                         <th>Estado</th>
                         <th>Acciones</th>
                     </tr>
-                    </thead>
-                    <tbody class="">
-
+                </thead>
+                <tbody class="">
                     <tr v-for="(user, index) in users" :key="index">
-                        <td>@{{ user.name_1 + ' ' + user.name_2 }}</td>
-                        <td>@{{ user.surname_1 + ' ' + user.surname_2 }}</td>
+                        <td>@{{ user.full_name }}</td>
                         <td>@{{ user.phone }}</td>
                         <td>@{{ user.email }}</td>
                         <td>@{{ user.active == true ? 'Activo' : 'Inactivo'}}</td>
                         <td>
                             <div class="d-flex gap-2">
-                                <button type="button" class="btn btn-sm btn-warning"
-                                        data-toggle="tooltip" data-placement="top" title="Editar Usuario"
-                                        @click="openModalEditUser(user)">
-                                    <i class="fas fa-edit"></i>
+                                <button type="button" class="btn btn-table "
+                                    :class="user.active ? 'btn-color-table-active' : 'btn-color-table-inactive'"
+                                    data-toggle="tooltip" data-placement="top" @click="toggleStatus"
+                                    :title="user.active ? 'Inactivar usuario' : 'Activar usuario'">
+                                    <i :class="`fas ${user.active ? 'fa-check' : 'fa-times mx-3px'}`"></i>
                                 </button>
-                                <button type="button" class="btn btn-sm btn-info"
-                                        data-toggle="tooltip" data-placement="top"
-                                        @click="openModalPermissions"
-                                        title="Permisos Usuario">
+                                <button type="button" class="btn btn-color-table-edit btn-table" data-toggle="tooltip"
+                                    data-placement="top" title="Editar Usuario" @click="openModalEditUser(user)">
+                                    <i class="fas fa-pencil-alt"></i>
+                                </button>
+                                <button type="button" class="btn btn-color-table-permissions btn-table" data-toggle="tooltip"
+                                    data-placement="top" title="Permisos Usuario" @click="openModalPermissions">
                                     <i class="fas fa-key"></i>
                                 </button>
+
                             </div>
                         </td>
                     </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="d-flex justify-content-center align-items-center" v-else>
-                @include('preloader')
-            </div>
+                </tbody>
+            </table>
         </div>
-        @include('User.Modals.create_user')
-        @include('User.Modals.edit_user')
-        @include('User.Modals.permissions')
+        <div class="d-flex justify-content-center align-items-center" v-else>
+            @include('preloader')
+        </div>
     </div>
+    @include('User.Modals.create_user')
+    @include('User.Modals.edit_user')
+    @include('User.Modals.permissions')
+</div>
 
-    <script>
-        const app = Vue.createApp({
+<script>
+    const app = Vue.createApp({
             data() {
                 return {
                     isLoading: false,
-                    users: null,
+                    users: [],
                     createUser: {
-                        firstName: '',
-                        secondName: '',
-                        firstSurname: '',
-                        secondSurname: '',
+                        fullName: '',
                         telephone: '',
                         email: '',
                         password: '',
@@ -87,15 +89,22 @@
                     },
                     editUser: {
                         id: '',
-                        firstName: '',
-                        secondName: '',
-                        firstSurname: '',
-                        secondSurname: '',
+                        fullName: '',
                         telephone: '',
                         email: '',
                         isActive: null
                     },
-                    fieldsStatus: {
+                    fieldsStatus: this.initializeFields(),
+                    fieldsStatusEdit: this.initializeFields(),
+                    fetchErrors: [],
+                }
+            },
+            mounted() {
+                this.getUsers();
+            },
+            methods: {
+                initializeFields() {
+                    return {
                         firstName: false,
                         secondName: false,
                         firstSurname: false,
@@ -104,22 +113,9 @@
                         email: false,
                         password: false,
                         password_confirmation: false,
-                    },
-                    fieldsStatusEdit: {
-                        firstName: false,
-                        secondName: false,
-                        firstSurname: false,
-                        secondSurname: false,
-                        telephone: false,
-                        email: false,
-                    },
-                    fetchErrors: [],
-                }
-            },
-            mounted() {
-                this.getUsers();
-            },
-            methods: {
+                    };
+                },
+
                 openModalCreateUser() {
                     this.fetchErrors = []
                     $('#createUser').modal('show');
@@ -127,53 +123,49 @@
 
                 async getUsers() {
                     this.isLoading = true;
-                    if ($.fn.DataTable.isDataTable('#tableUsers')) {
-                        let table = $('#tableUsers').DataTable();
-                        table.destroy();
-                    }
                     try {
-                        const res = await fetch('{{ route('User.AllUsers') }}', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'accept': 'application/json',
-                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                            },
-                        });
-                        const response = await res.json();
+                        dataTableUtils.destroyDataTable('tableUsers')
+
+                        const response = await fetchUtils.fetchGet('{{ route('User.AllUsers') }}');
+
                         this.isLoading = false;
+
                         if (!response.success) {
                             alert('Fallo en la peticion: ', response.message);
+                            return;
                         }
+
                         this.users = response.users;
-                        await this.executeDataTable();
-                    } catch (e) {
+
+                        await this.$nextTick();
+                        dataTableUtils.initializeDataTable('tableUsers')
+             
+                    } catch (error) {
                         this.isLoading = false;
-                        alert('Error al obtener usuarios: ' + e);
+                        alert('Error al obtener usuarios: ' + error.message);
                     }
                 },
 
-                async storeUser() {
+                async submintFormStoreUser() {
                     const btn = $('#btnCreateUser')
                     btn.loading()
                     try {
-                        const res = await fetch('{{ route('User.Create') }}', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'accept': 'application/json',
-                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                            },
-                            body: JSON.stringify(this.createUser)
-                        });
-                        const response = await res.json()
+                        const response = await fetchUtils.fetchPost(
+                            '{{ route('User.Create') }}',
+                            '{{ csrf_token() }}',
+                            this.createUser
+                        );
+                       
                         btn.unLoading()
+
                         if (!response.success) {
-                            utilities.toastr_('error', 'Alerta', 'Error al almacenar el usuario: ' + response.message);
-                            this.validateFieldsStatus(response.errors);
-                            return false;
+                            fetchUtils.validateFields(response.errors, this.fieldsStatus, this.fetchErrors);
+                            utilities.toastr_('error', 'Alerta', 'Error al almacenar el usuario');
+                            return;
                         }
+
                         await this.getUsers();
+
                         $('#createUser').modal('hide');
                         utilities.toastr_('success', response.message);
                     } catch (e) {
@@ -185,10 +177,7 @@
                 openModalEditUser(user) {
                     this.fetchErrors = []
                     this.editUser.id = user.id;
-                    this.editUser.firstName = user.name_1;
-                    this.editUser.secondName = user.name_2;
-                    this.editUser.firstSurname = user.surname_1;
-                    this.editUser.secondSurname = user.surname_2;
+                    this.editUser.firstName = user.full_name;
                     this.editUser.telephone = user.phone;
                     this.editUser.email = user.email;
                     this.editUser.isActive = user.active;
@@ -199,26 +188,23 @@
                     const btn = $('#btnEditUser');
                     btn.loading();
                     try {
-                        const res = await fetch('{{ route('User.Update') }}', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'accept': 'application/json',
-                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                            },
-                            body: JSON.stringify(this.editUser)
-                        });
-                        const response = await res.json();
-                        console.log(response)
+                        const response = await fetchUtils.fetchPost(
+                            '{{ route('User.Update') }}',
+                            '{{ csrf_token() }}',
+                            this.editUser
+                        );
+
                         btn.unLoading();
                         if (!response.success) {
+                            fetchUtils.validateFields(response.errors, this.fieldsStatus, this.fetchErrors);
                             utilities.toastr_('error', 'Alerta', 'Error al almacenar el usuario: ' + response.message);
-                            this.validateFieldsEdit(response.errors);
-                            return false;
+                            return;
                         }
+
                         await this.getUsers()
-                        $('#editUserModal').modal('hide')
+
                         utilities.toastr_('success', 'Exito', response.message)
+                        $('#editUserModal').modal('hide')
                     } catch (e) {
                         btn.unLoading();
                         alert('Error al actulizar: ' + e);
@@ -229,32 +215,19 @@
                     $('#permissionsModal').modal('show')
                 },
 
-                async executeDataTable() {
-                    await this.$nextTick();
-                    $('#tableUsers').DataTable({
-                        language: es_datatables,
-                        retrieve: true,
-                        order: []
-                    });
+
+                toggleStatus () {
+
                 },
 
-                validateFieldsStatus(errors) {
-                    this.fetchErrors = errors;
-                    Object.keys(this.fetchErrors).forEach(key => {
-                        this.fieldsStatus[key] = true;
-                    })
-                },
-
-                validateFieldsEdit(errors) {
-                    this.fetchErrors = errors;
-                    Object.keys(this.fetchErrors).forEach(key => {
-                        this.fieldsStatusEdit[key] = true;
-                    })
-                },
+                togglePassword(nameInput) {
+                    const passwordInput = document.getElementById(nameInput);
+                    const isPasswordVisible = passwordInput.type === 'password';
+                    passwordInput.type = isPasswordVisible ? 'text' : 'password';
+                    this.textContent = isPasswordVisible ? 'Hide' : 'Show';
+                }
             }
         });
         const vm = app.mount('#app');
-    </script>
+</script>
 @stop
-
-
