4b410a94c21493dd54a4e404a630d6b208c3359a
Camilo Alarcon <camilo4750@gmail.com>
2024-12-03 15:56:40 -0500

    Fix utilities remove try-catch

--- public/js/FetchUtils.js
+++ public/js/FetchUtils.js
@@ -1,59 +1,44 @@
 const fetchUtils = {
-    async fetchGet(url,  additionalHeaders = {}) {
+    async fetchGet(url, additionalHeaders = {}) {
         const defaultHeaders = {
             "Content-Type": "application/json",
-            "accept": "application/json",
+            accept: "application/json",
         };
 
-        try {
-            const res = await fetch(url, {
-                method: "GET",
-                headers: { ...defaultHeaders, ...additionalHeaders },
-            });
+        const res = await fetch(url, {
+            method: "GET",
+            headers: { ...defaultHeaders, ...additionalHeaders },
+        });
 
-            const data = await res.json();
+        const data = await res.json();
 
-            if (!res.ok) {
-                throw new Error(data.message || "Error en la solicitud");
-            }
-
-            return data;
-        } catch (error) {
-            console.error(`Error en la petición a ${url}:`, error);
-            throw error;
+        if (!res.ok) {
+            throw new Error(data.message || "Error en la solicitud");
         }
+
+        return data;
     },
 
     async fetchPost(url, csrfToken, body = null, additionalHeaders = {}) {
         const defaultHeaders = {
             "Content-Type": "application/json",
-            "accept": "application/json",
+            accept: "application/json",
             "X-CSRF-TOKEN": csrfToken,
         };
 
-        try {
-            const res = await fetch(url, {
-                method: "POST",
-                headers: { ...defaultHeaders, ...additionalHeaders },
-                body: JSON.stringify(body),
-            });
+        const res = await fetch(url, {
+            method: "POST",
+            headers: { ...defaultHeaders, ...additionalHeaders },
+            body: JSON.stringify(body),
+        });
 
-            const data = await res.json();
+        const data = await res.json();
 
-            if (!res.ok) {
-                throw new Error(data.message || "Error en la solicitud");
-            }
-
-            return data;
-        } catch (error) {
-            console.error(`Error en la petición POST a ${url}:`, error);
-            throw error;
-        }
+        return data;
     },
 
-
     validateFields(errors, fieldsStatus, fetchErrors) {
-        Object.keys(errors).forEach(key => {
+        Object.keys(errors).forEach((key) => {
             fieldsStatus[key] = true;
             fetchErrors[key] = errors[key];
         });
