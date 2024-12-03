17f9edfbd5276a2853e15b4eefeeb9dc84079178
Camilo Alarcon <camilo4750@gmail.com>
2024-12-03 15:57:03 -0500

    Add middleware

--- routes/App/UserRoute.php
+++ routes/App/UserRoute.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\User\UserController;
-Route::prefix('usuarios')->group(function () {
+Route::middleware(['auth'])->prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('User.Index');
     Route::get('getUsers', [UserController::class, 'allUsers'])->name('User.AllUsers');
     Route::post('/store', [UserController::class, 'store'])->name('User.Create');
