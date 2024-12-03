2b5b2a67c44a36f56d93d61edcb3aa6a561fde8b
Camilo Alarcon <camilo4750@gmail.com>
2024-12-03 16:10:31 -0500

    Add email to dropdown

--- resources/views/vendor/adminlte/partials/navbar/menu-item-fullscreen-widget.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-fullscreen-widget.blade.php
@@ -10,8 +10,10 @@
             aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-user-circle"></i>
         </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <form method="POST" action="{{ route('logout') }}">
+        <div class="dropdown-menu p-2" aria-labelledby="dropdownMenuButton">
+            <p class="mb-0">{{Auth::user()->email}}</p>
+            <hr class="my-2">
+            <form method="POST" action="{{ route('logout') }}" class="mb-0">
                 @csrf
                 <button class="dropdown-item d-flex align-items-center" type="submit">
                     Cerrar session <i class="fas fa-power-off ml-2 text-red"></i>
