cc12375044845dac2e032811649da0f04e5b8d12
Camilo Alarcon <camilo4750@gmail.com>
2024-12-04 12:08:27 -0500

    Creating tests for the update process

--- tests/Feature/User/UserTest.php
+++ tests/Feature/User/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\User;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Session;
 use Tests\BaseTest;
@@ -9,7 +10,7 @@ use Tests\BaseTest;
 class UserTest extends BaseTest
 {
     use DatabaseTransactions;
-    
+
     /**
      * @test
      */
@@ -33,6 +34,39 @@ class UserTest extends BaseTest
             'full_name' => 'Nombre de prueba 2',
             'email' => 'prueba2@workshop.com',
             'phone' => '53455653',
+            'active' => 1,
+        ]);
+    }
+
+
+    /**
+     * @test
+     */
+    public function is_update_working()
+    {
+        $user = User::factory()->create([
+            'full_name' => 'Nombre Original',
+            'email' => 'original@workshop.com',
+            'phone' => '12345678',
+            'active' => 1,
+        ]);
+
+        $this->actingAs($this->user);
+        $response = $this->post(route('User.Update', $user->id), [
+            'full_name' => 'Nombre Actualizado',
+            'email' => 'prueba@workshop.com',
+            'phone' => '34344444',
+            'active' => 1,
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertNull(Session::get('errors'));
+        $response->assertJsonStructure(['success', 'message']);
+
+        $this->assertDatabaseHas('users', [
+            'full_name' => 'Nombre Actualizado',
+            'email' => 'prueba2@workshop.com',
+            'phone' => '34344444',
             'active' => 1,
         ]);
     }
--- tests/Integration/Repositories/User/UserRepositoryTest.php
+++ tests/Integration/Repositories/User/UserRepositoryTest.php
@@ -1,15 +1,21 @@
-<?php 
+<?php
 
 namespace Tests\Integration\Repositories\User;
 
 use App\Dto\user\UserNewDto;
+use App\Dto\User\UserUpdateDto;
 use App\Interfaces\Repositories\User\UserRepositoryInterface;
+use App\Models\User;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
 use Tests\BaseTest;
 
-class UserRepositoryTest extends BaseTest 
+class UserRepositoryTest extends BaseTest
 {
+
+    /**
+     * @test
+     */
     public function is_store_repo()
     {
         $this->actingAs($this->user);
@@ -31,6 +37,43 @@ class UserRepositoryTest extends BaseTest
             'full_name' => 'Nombre de prueba 2',
             'email' => 'prueba3@workshop.com',
             'phone' => '53455653',
+            'active' => 1,
+        ]);
+    }
+
+
+    /**
+     * @test
+     */
+    public function is_update_repo()
+    {
+        $this->actingAs($this->user);
+
+        $existingUser = User::factory()->create([
+            'full_name' => 'Nombre de prueba',
+            'email' => 'prueba@workshop.com',
+            'phone' => '12345678',
+            'active' => 1,
+        ]);
+
+        $userUpdateDto = new UserUpdateDto();
+        $userUpdateDto->id = $existingUser->id;
+        $userUpdateDto->full_name = "Nombre de prueba actualizado";
+        $userUpdateDto->email = "prueba_actualizada@workshop.com";
+        $userUpdateDto->phone = "87654321";
+        $userUpdateDto->active = 1;
+
+        (App::make(UserRepositoryInterface::class))
+            ->setUser($this->user)
+            ->update($userUpdateDto);
+
+        $this->assertNull(Session::get('errors'));
+
+        $this->assertDatabaseHas('users', [
+            'id' => $existingUser->id,
+            'full_name' => 'Nombre de prueba actualizado',
+            'email' => 'prueba_actualizada@workshop.com',
+            'phone' => '87654321',
             'active' => 1,
         ]);
     }
