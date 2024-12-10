5edd49dce0176ae858ebf8b2adbf38044066d70b
Camilo Alarcon <camilo4750@gmail.com>
2024-12-10 17:27:16 -0500

    Add niddleware

--- routes/App/UserRoute.php
+++ routes/App/UserRoute.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\User\UserController;
-Route::middleware(['auth'])->prefix('users')->group(function () {
+Route::middleware(['auth', 'session.timeout'])->prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('User.Index');
     Route::get('getUsers', [UserController::class, 'allUsers'])->name('User.AllUsers');
     Route::post('/store', [UserController::class, 'store'])->name('User.Create');
