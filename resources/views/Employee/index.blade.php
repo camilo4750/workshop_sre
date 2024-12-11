a002aef9dbb9a61731f27e43a0ec9ecc3e1dbbfa
Camilo Alarcon <camilo4750@gmail.com>
2024-12-11 17:29:46 -0500

    Add base index

--- resources/views/Employee/index.blade.php
+++ resources/views/Employee/index.blade.php
@@ -7,10 +7,30 @@
 @stop
 @section('content')
 <div id="app">
+    <div class="card border-color-1 p-2">
+        <div class="d-flex justify-content-end mb-2">
+            <button class="btn btn-secondary">Crear empleado</button>
+        </div>
+        <div class="my-3" v-if="!isLoading">
 
+        </div>
+    </div>
 </div>
 
 <script>
-    
+     const app = Vue.createApp({
+            data() {
+                return {
+                    isLoading: false,
+                }
+            },
+            mounted() {
+
+            },
+            methods: {
+                
+            }
+        });
+        const vm = app.mount('#app');
 </script>
 @stop
