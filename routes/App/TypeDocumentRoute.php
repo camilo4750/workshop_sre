11b4337a7173859accb1fb0263724b9c3707bc50
Camilo Alarcon <camilo4750@gmail.com>
2024-12-16 10:11:35 -0500

    Fix name route

--- routes/App/TypeDocumentRoute.php
+++ routes/App/TypeDocumentRoute.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Lists\TypeDocument\TypeDocumentController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('typeDocument')->group(function () {
-    Route::get( '/getAll', [TypeDocumentController::class, 'getAll'])->name('typeDocument.GetAll');
+    Route::get( '/getAll', [TypeDocumentController::class, 'getAll'])->name('TypeDocument.GetAll');
 });
