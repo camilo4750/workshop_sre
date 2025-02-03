d588dda8938fd0ff5d51cea051b7a9054a35f061
Camilo Alarcon <camilo4750@gmail.com>
2025-02-03 18:40:41 -0500

    Add process to skip csrf in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Session;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Session::start();
+        $this->withoutMiddleware([VerifyCsrfToken::class]);
+    }
 }
