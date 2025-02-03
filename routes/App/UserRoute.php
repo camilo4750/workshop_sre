046cf5147f5da56174af3c733aa1ae23434516c8
Camilo Alarcon <camilo4750@gmail.com>
2025-02-03 18:45:21 -0500

    Remove middleware

--- routes/App/UserRoute.php
+++ routes/App/UserRoute.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\User\UserController;
-Route::middleware(['auth', 'session.timeout'])->prefix('users')->group(function () {
+use App\Http\Controllers\User\UserController;
+
+Route::middleware('auth')->prefix('users')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('User.Index');
     Route::get('getUsers', [UserController::class, 'allUsers'])->name('User.AllUsers');
     Route::post('/store', [UserController::class, 'store'])->name('User.Create');
