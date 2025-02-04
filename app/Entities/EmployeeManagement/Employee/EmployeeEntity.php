99bd792644432f2534e047606f6adebb5318ad98
Camilo Alarcon <camilo4750@gmail.com>
2025-02-04 16:25:22 -0500

    Add factory specification

--- app/Entities/EmployeeManagement/Employee/EmployeeEntity.php
+++ app/Entities/EmployeeManagement/Employee/EmployeeEntity.php
@@ -3,6 +3,7 @@
 namespace App\Entities\EmployeeManagement\Employee;
 
 use App\Entities\CoreEntity;
+use Database\Factories\EmployeeFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class EmployeeEntity extends CoreEntity
@@ -10,4 +11,9 @@ class EmployeeEntity extends CoreEntity
     use HasFactory;
 
     protected $table = 'employees';
+
+    protected static function newFactory()
+    {
+        return EmployeeFactory::new();
+    }
 }
